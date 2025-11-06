5b34db63294325ecb3636b87834f4a2b8253a4d8
LkRonald <rpereirac@unicartagena.co>
2025-11-06 13:33:58 -0500

    feat: se agrego la clase AplicarDescuento

---
+++ Taller-16/Ejercicio1/src/main/java/AplicarDescuento.java
@@ -0,0 +1,6 @@
+public class AplicarDescuento {
+
+    public void aplicarDescuento(Descuento descuento, double valorOriginal) {
+        System.out.println("El edesuebto es de: " + descuento.aplicarDecuento(valorOriginal));
+    }
+}
